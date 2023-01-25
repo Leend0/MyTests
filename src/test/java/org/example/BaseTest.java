61feff64a509f2aa123a2dc28a54fd68f4dd00c4
Lindokuhle <Lindokuhle@digilink.africa>
2023-01-25 10:33:05 +0200

    first commit

---
+++ src/test/java/org/example/BaseTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BaseTest
+{
+
+    @Test
+    void setUp()
+    {
+        System.out.println();
+    }
+
+
+}
